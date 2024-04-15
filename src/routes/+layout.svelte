26f99194591ba6d8f682e449e89a3516521802ad
Chanawat Voravijitchaikul <89173727+Chanawatv@users.noreply.github.com>
2024-04-15 12:23:36 +0700

    create home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import './styles.css';
+</script>
+
+<div class="background">
+    <slot/>
+</div>
+
+<style>
+    .background {
+        width: 100%;
+        height: 100%;
+        background: 
+            radial-gradient(ellipse, transparent 70%, rgba(24, 47, 40, 0.1) 90%, rgba(24, 47, 40, 0.4) 100%),
+            /* radial-gradient(circle at center var(calc(100%+2px)), rgb(146, 180, 79) 20px, rgb(172, 219, 79) 400px, transparent 0%), */
+            radial-gradient(circle at center 130%, var(--background-primary) 0%, var(--background-secondary) 145%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,154 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { goto } from '$app/navigation';
+
+    function startQuiz() {
+        goto('/quiz');
+    }
+</script>
+
+<main>
+    <h2 class=verse-1>
+        Roses are <span class=red>red</span>,
+        <div class=ribbon-top></div>
+        <div class=ribbon-bottom></div>
+    </h2>
+    <h2 class=verse-2>
+        Violets are <span class=blue>blue</span>,
+        <div class=ribbon-top></div>
+        <div class=ribbon-bottom></div>
+    </h2>
+    <h3 class=pre-chorus>
+        Let's find out,
+        <div class=arrow></div>
+    </h3>
+    <h1 class=chorus>What flower are you?</h1>
+    <button class=start-button on:click={startQuiz}>Begin Exploring &gt;&gt;</button>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        white-space: nowrap;
+        transform: skewY(5deg) translateY(-14px);
+    }
+    h1, h2, h3 {
+        margin: 0;
+    }
+    .red {
+        color: var(--text-red);
+    }
+    .blue {
+        color: var(--text-blue);
+    }
+    .verse-1 {
+        font-size: 2.4em;
+        color: var(--text-primary);
+        transform: translate(-3.9em, 2.2rem);
+        padding: 0 1.8rem 0 1.4rem;
+        background-color: var(--text-primary-background);
+    }
+    .verse-1 .ribbon-bottom {
+        width: 0;
+        height: 0;
+        border-left: 50px solid transparent;
+        border-bottom: 50px solid var(--text-primary-background);
+        position: absolute;
+        bottom: 0;
+        left: -30px;
+    }
+    .verse-1 .ribbon-top {
+        width: 0;
+        height: 0;
+        border-left: 50px solid transparent;
+        border-top: 50px solid var(--text-primary-background);
+        position: absolute;
+        top: 0;
+        left: -30px;
+    }
+    .verse-2 {
+        font-size: 2.4em;
+        color: var(--text-primary);
+        transform: translateX(4em);
+        padding: 0 1rem;
+        background-color: var(--text-primary-background);
+    }
+    .verse-2 .ribbon-bottom {
+        width: 0;
+        height: 0;
+        border-right: 50px solid transparent;
+        border-bottom: 50px solid var(--text-primary-background);
+        position: absolute;
+        bottom: 0;
+        right: -30px;
+    }
+    .verse-2 .ribbon-top {
+        width: 0;
+        height: 0;
+        border-right: 50px solid transparent;
+        border-top: 50px solid var(--text-primary-background);
+        position: absolute;
+        top: 0;
+        right: -30px;
+    }
+    .pre-chorus {
+        font-size: 1.8em;
+        color: var(--text-secondary);
+        padding: 0 10px;
+        transform: translate(-8.6em, 0.4rem);
+        background-color: var(--text-secondary-background);
+        box-shadow: var(--text-box-shadow) 7px -7px;
+        border-radius: 10px;
+    }
+    .pre-chorus .arrow {
+        width: 0;
+        height: 0;
+        border-left: 20px solid transparent;
+        border-top: 10px solid var(--text-secondary-background);
+        position: absolute;
+        bottom: -9px;
+        left: 80%;
+    }
+    .chorus {
+        font-size: 4em;
+        font-weight: bold;
+        color: var(--text-primary);
+        padding: 0 20px 5px 20px;
+        background-color: var(--text-primary-background);
+        box-shadow: var(--text-box-shadow) 6px -6px;
+        border-radius: 20px;
+    }
+    .start-button {
+        font-family: cursive, monospace;
+        font-size: 1.6em;
+        font-weight: bold;
+        padding: 0px 20px 5px 20px;
+        background-color: var(--text-box-shadow);
+        color: var(--text-secondary);
+        border: none;
+        border-radius: 10px;
+        box-shadow: 
+            var(--text-primary-background) 6px -6px,
+            var(--text-box-shadow) 12px -12px;
+        transform: translateX(6em);
+        z-index: -1;
+        cursor: pointer;
+    }
+    @media (max-width: 800px) {
+        main {
+            transform: scale(0.8) skewY(5deg) translateY(-14px);
+        }
+    }
+    @media (max-width: 600px) {
+        main {
+            transform: scale(0.6) skewY(5deg) translateY(-14px);
+        }
+    }
+    @media (max-width: 450px) {
+        main {
+            transform: scale(0.4) skewY(5deg) translateY(-14px);
+        }
+    }
+</style>
